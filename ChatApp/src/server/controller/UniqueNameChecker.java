bd1f93ea6b1690d75a59953820de491c395d4f1e
Jason Seldon Tumblin <jtumbli1@my.westga.edu>
2018-03-26 18:13:36 -0400

    Created an IO for userName storage
    Created the ability to check if userName is unique.

---
+++ ChatApp/src/server/controller/UniqueNameChecker.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package server.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import server.io.UsernameIO;
+
+/**
+ * @author jason
+ *
+ */
+public class UniqueNameChecker {
+	
+private ArrayList<String> takenUserNames;
+	
+	/**
+	 * Initializer gets a collection of userNames from file in resources folder using UsernameIO.LoadUserNames().
+	 */
+	public UniqueNameChecker() {
+		try {
+			this.takenUserNames = UsernameIO.loadUserNames();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			this.takenUserNames = new ArrayList<>();
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * Checks specified userName against all current userNames in server for uniqueness. 
+	 * 
+	 * @param userName The userName to check against servers userNames
+	 * @return true if userName is unique, false otherwise.
+	 */
+	public boolean isUniqueName(String userName) {
+				
+		for (String takenuserName : takenUserNames) {
+			if (!this.checkUniqueCharacters(takenuserName, userName)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	private boolean checkUniqueCharacters(String takenUsername, String newUsername) {
+		
+		Character takenUserCharacter = '&';
+		Character newUserCharacter = '&';
+		
+		if(takenUsername.length() > 0) {
+			takenUserCharacter = takenUsername.charAt(0);
+		}
+		
+		if(newUsername.length() > 0) {
+			newUserCharacter = newUsername.charAt(0);
+		}
+		
+		if(!takenUserCharacter.equals(newUserCharacter)) {
+			return true;
+		}
+		
+		if(takenUserCharacter.equals('&') && newUserCharacter.equals('&')) {
+			return false;
+		}
+		
+		takenUsername = takenUsername.substring(1);
+		newUsername = newUsername.substring(1);
+		
+		return checkUniqueCharacters(takenUsername, newUsername);
+		
+	}
+}
---
+++ ChatApp/src/server/io/UsernameIO.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package server.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * @author jason
+ *
+ */
+public class UsernameIO {
+private static final String NEW_LINE_SEPARATOR = System.lineSeparator();
+	
+	public static void saveUserName(String userName) throws IOException {
+		String path = "resources" + File.separator + "usernames.txt";
+		
+		FileWriter fileWriter = new FileWriter(path);
+		
+		fileWriter.append(userName);
+		fileWriter.append(NEW_LINE_SEPARATOR);
+		fileWriter.flush();
+		fileWriter.close();
+	}
+	
+	public static ArrayList<String> loadUserNames() throws IOException {
+		String path = "resources" + File.separator + "usernames.txt";
+		
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+		String userName = "";
+		ArrayList<String> userNames = new ArrayList<>();
+		
+		while((userName = bufferedReader.readLine()) != null) {
+			userNames.add(userName);
+		}
+		
+		bufferedReader.close();
+		
+		return userNames;
+	}
+}
